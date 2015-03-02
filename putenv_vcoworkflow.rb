4f38f24bca604a3966d931b349aee179bb50afe8
Gregory Ruiz-Ade <gkra@unnerving.org>
2015-03-02 11:34:55 -0800

    Pull parameter setting into separate method

--- putenv_vcoworkflow.rb
+++ putenv_vcoworkflow.rb
@@ -61,33 +61,24 @@ module Putenv
         # Get the workflow
         wf = VcoWorkflows::Workflow.new(env['orchestrator']['workflow_name'], wfoptions)
 
-        # Grab the service to make subsequent workflow calls simpler
-        wf_service = wf.service
-        wf_id = wf.id
-
         # For every component we're going to provision, translate the
         # attributes we need into a form for the underlying engine
         # (in this case, vcoworkflows gem), then execute it.
         env['components'].each do |name, component|
-          # Build up a parameter array for the workflow from our environment
-          params = {}
-          params['component']    = name
-          params['businessUnit'] = env['product']
-          params['environment']  = env['environment']
-          params['onBehalfOf']   = component['execute_on_behalf_of']
-          params['reservation']  = component['reservation_policy']
-          params['coreCount']    = component['compute']['cpu']
-          params['ramMB']        = component['compute']['memory']
-          params['image']        = component['compute']['image']
-          params['location']     = component['location']
-          params['runlist']      = component['run_list'] + [component['component_role']]
-          params['machineCount'] = component['count']
-
-          # Get the workflow
-          wf = VcoWorkflows::Workflow.new(workflow, id: wf_id, service: wf_service)
+          # Get the workflow.
+          # If the component workflow name is the same as the last workflow
+          # we fetched, re-use the name and id to save on some API calls.
+          # If it's not, see if there's an ID specified and get the new
+          # workflow.
+          if wf.name.eql?(component['workflow_name'])
+            wf = VcoWorkflows::Workflow.new(wf.name, id: wf.id, service: wf.service)
+          else
+            id = component['workflow_id'] ? nil : component['workflow_id']
+            wf = VcoWorkflows::Workflow.new(component['workflow_name'], id: id, service: wf.service)
+          end
 
           # Set the parameters
-          wf.parameters = params
+          wf.parameters = set_parameters(name, component)
 
           # Execute the workfow and grab the execution ID for later use
           running_jobs << wf.execute
@@ -97,6 +88,29 @@ module Putenv
         running_jobs.each do |job|
           puts "  - #{job}"
         end
+      end
+
+      # rubocop: disable LineLength
+
+      # set_parameters - Set up the input parameter hash for the workflow from
+      # our environment definition for the current component.
+      # @param [String] name Name of the current component
+      # @param [Hash] component Hash of component definition data
+      # @return [Hash] Input parameter hash for given to the workflow object
+      def set_parameters(name = nil, component = nil)
+        params = {}
+        params['component']    = name
+        params['businessUnit'] = env['product']
+        params['environment']  = env['environment']
+        params['onBehalfOf']   = component['execute_on_behalf_of']
+        params['reservation']  = component['reservation_policy']
+        params['coreCount']    = component['compute']['cpu']
+        params['ramMB']        = component['compute']['memory']
+        params['image']        = component['compute']['image']
+        params['location']     = component['location']
+        params['runlist']      = component['run_list'] + [component['component_role']]
+        params['machineCount'] = component['count']
+        params
       end
     end
   end
